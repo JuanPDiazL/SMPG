eee4a4d1107ee15a26da8158f5d84fcf6754d328
JuanPDiazL <juan.lombana@correounivalle.edu.co>
2024-06-27 13:21:02 -0500

    better handling of duplicated place names

--- nsmpgCore/parsers/CSVParser.py
+++ nsmpgCore/parsers/CSVParser.py
@@ -4,17 +4,17 @@ import pandas as pd
 def parse_csv(filename:str):
     df = pd.read_csv(filename, header=0, index_col=0)
 
+    has_duplicates = not df.index.is_unique
+    timestamps = df.columns.to_list()
     dataset_context = dict_of_nparrays(df)
-    rows = df.head()
-    has_duplicates = rows.duplicated().any()
-    return dataset_context, df.columns.to_list(), has_duplicates
+    return dataset_context, timestamps, has_duplicates
 
 # converts a pandas dataframe to a dictionary of numpy arrays
 def dict_of_nparrays(df:pd.DataFrame):
     dataset = {}
+    df = df.groupby(level=0).first() # removes duplicates
     dft = df.T
     for col_index in dft.columns:
         dataset[str(col_index)] = dft[col_index].to_numpy()
 
-    # print(dataset)
     return dataset
