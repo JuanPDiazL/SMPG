3f6a05397e1278ea86bd712398246f9b502d1ce2
JuanPDiazL <juan.lombana@correounivalle.edu.co>
2024-07-30 21:22:39 -0500

    map selection is now more intuitive and some refactoring were applied

--- map_settings_dialog.py
+++ map_settings_dialog.py
@@ -11,6 +11,7 @@ from qgis.PyQt.QtWidgets import (
     QComboBox,
     QLabel,
     QListWidget,
+    QRadioButton,
 )
 
 from .nsmpgCore.pyqgis_utils import (
@@ -25,8 +26,6 @@ from qgis.core import (
 
 MAP_SETTINGS_DIALOG_CLASS,_ = uic.loadUiType(os.path.join(
     os.path.dirname(__file__), 'map_settings_dialog.ui'))
-
-IMPORT_SHP_TEXT = 'Import map from shapefile...'
 
 class MapSettingsDialog(QDialog, MAP_SETTINGS_DIALOG_CLASS):
 
@@ -55,6 +54,8 @@ class MapSettingsDialog(QDialog, MAP_SETTINGS_DIALOG_CLASS):
             'join_field': '',
         }
 
+        self.useProjectLayerRadioButton: QRadioButton
+        self.useShapefileRadioButton: QRadioButton
         self.mapSelectionComboBox: QComboBox
         self.loadShapefileButton: QPushButton
         self.shapefilePathLineEdit: QLineEdit
@@ -67,14 +68,17 @@ class MapSettingsDialog(QDialog, MAP_SETTINGS_DIALOG_CLASS):
         self.targetLabel: QLabel
         self.targetFieldComboBox: QComboBox
 
-        self.import_widgets = [self.loadShapefileButton, self.shapefilePathLineEdit]
+        self.import_shp_widgets = [self.loadShapefileButton, self.shapefilePathLineEdit]
+        self.use_project_layer_widgets = [self.mapSelectionComboBox]
 
+        self.useProjectLayerRadioButton.toggled.connect(self.source_selection_rb_event)
+        self.useShapefileRadioButton.toggled.connect(self.source_selection_rb_event)
         self.mapSelectionComboBox.currentIndexChanged.connect(self.map_selection_combobox_event)
         self.loadShapefileButton.clicked.connect(self.load_shapefile_button_event)
         self.addButton.clicked.connect(lambda: self.list_move_element(self.blackList, self.whiteList))
         self.removeButton.clicked.connect(lambda: self.list_move_element(self.whiteList, self.blackList))
 
-        self.set_show_import_widgets(False)
+        self.source_selection_rb_event()
         
     def list_move_element(self, source_list: QListWidget, destination_list: QListWidget):
         for item in source_list.selectedItems():
@@ -82,29 +86,19 @@ class MapSettingsDialog(QDialog, MAP_SETTINGS_DIALOG_CLASS):
             destination_list.addItem(source_list.takeItem(item_index))
 
     def map_selection_combobox_event(self, index):
-        if index == 0: 
-            self.set_show_import_widgets(True)
-            return
-        else: self.set_show_import_widgets(False)
+        self.shapefilePathLineEdit.setText("")
+        self.temp_map_layer = self.map_relations[index][0]
+        self.update_target_field_combobox_content()
 
-        if index > 0:
-            selected_map_index = max(0, index - 1)
-            self.temp_map_layer = self.map_relations[selected_map_index][0]
-
-        self.update_target_field_combobox()
-
-    def update_map_combobox(self):
-        maps: dict[QgsVectorLayer] = get_vector_layers()
+    def update_map_combobox_content(self):
+        maps = get_vector_layers()
         self.map_relations = [(map, map.id(), map.name()) for map in maps]
+        map_names = [item[2] for item in self.map_relations]
 
-        map_names = [IMPORT_SHP_TEXT] + [item[2] for item in self.map_relations]
-
-        default_selection = min(1, map_names.__len__())
         self.mapSelectionComboBox.clear()
         self.mapSelectionComboBox.addItems(map_names)
-        self.mapSelectionComboBox.setCurrentIndex(default_selection)
 
-    def update_target_field_combobox(self):
+    def update_target_field_combobox_content(self):
         fields = get_fields(self.temp_map_layer)
         self.targetFieldComboBox.clear()
         self.targetFieldComboBox.addItems(fields)
@@ -120,11 +114,18 @@ class MapSettingsDialog(QDialog, MAP_SETTINGS_DIALOG_CLASS):
         layer_preload = load_layer_file(self.shp_source)
         if not layer_preload.isValid(): return # continue if layer is valid
         self.temp_map_layer = layer_preload
-        self.update_target_field_combobox()
+        self.update_target_field_combobox_content()
+        self.mapSelectionComboBox.setCurrentIndex(-1)
         self.shapefilePathLineEdit.setText(self.shp_source)
 
-    def update_settings(self, settings: dict):
-        self.mapSelectionComboBox.setCurrentText(settings['selected_map'])
+    def update_input_states(self, settings: dict):
+        if settings['shp_source'] == "":
+            self.useProjectLayerRadioButton.setChecked(True)
+            self.update_map_combobox_content()
+            self.mapSelectionComboBox.setCurrentText(settings['selected_map'])
+        else:
+            self.useShapefileRadioButton.setChecked(True)
+            self.mapSelectionComboBox.setCurrentIndex(-1)
         self.shapefilePathLineEdit.setText(settings['shp_source'])
 
         self.blackList.clear()
@@ -137,33 +138,40 @@ class MapSettingsDialog(QDialog, MAP_SETTINGS_DIALOG_CLASS):
 
         self.targetFieldComboBox.setCurrentText(settings['join_field'])
 
-    def set_show_import_widgets(self, show=True):
-        if show: 
-            for w in self.import_widgets: w.show()
-        else: 
-            for w in self.import_widgets: w.hide()
+    def source_selection_rb_event(self):
+        if self.useProjectLayerRadioButton.isChecked():
+            for w in self.use_project_layer_widgets: w.show()
+            for w in self.import_shp_widgets: w.hide()
+        if self.useShapefileRadioButton.isChecked():
+            for w in self.import_shp_widgets: w.show()
+            for w in self.use_project_layer_widgets: w.hide()
 
     def get_list_items(self, list_widget: QListWidget):
         return [list_widget.item(i).text() for i in range(list_widget.count())]
 
     def showEvent(self, a0):
-        self.update_map_combobox()
-        self.update_settings(self.settings)
+        self.update_input_states(self.settings)
         return super().showEvent(a0)
     
     def accept(self) -> None:
-        temp_selected_map = self.mapSelectionComboBox.currentText()
-        if temp_selected_map == IMPORT_SHP_TEXT:
+        if self.useShapefileRadioButton.isChecked():
             shp_path = self.shapefilePathLineEdit.text()
             if shp_path == '' or not os.path.exists(shp_path):
                 QMessageBox.warning(self, 'Warning', 
                                     f'No valid shapefile was selected.',
                                     QMessageBox.Ok)
                 return
-            self.settings['selected_map'] = temp_selected_map
             self.settings['shp_source'] = shp_path
-        else: 
-            self.shapefilePathLineEdit.setText('')
+            self.settings['selected_map'] = ''
+        elif self.useProjectLayerRadioButton.isChecked():
+            selected_layer = self.mapSelectionComboBox.currentText()
+            if selected_layer == '':
+                QMessageBox.warning(self, 'Warning', 
+                                    f'No vector layer was selected.',
+                                    QMessageBox.Ok)
+                return
+            temp_selected_map_name = self.mapSelectionComboBox.currentText()
+            self.settings['selected_map'] = temp_selected_map_name
             self.settings['shp_source'] = ''
             
         temp_join_field = self.targetFieldComboBox.currentText()
