a6d5f3531cb3ca452b225a625d13687abbcec998
JuanPDiazL <juan.lombana@correounivalle.edu.co>
2024-08-03 12:04:00 -0500

    added docstrings

--- nsmpgCore/pyqgis_utils.py
+++ nsmpgCore/pyqgis_utils.py
@@ -10,22 +10,43 @@ from qgis.core import (
     QgsMessageLog,
 )
 
-def load_layer_file(source: str):
+def load_layer_file(source: str) -> QgsVectorLayer:
+    """Load a vector layer from a file.
+
+    Args:
+        source (str): The path to the layer file.
+
+    Returns:
+        QgsVectorLayer: The loaded layer.
+    """
     source = os.path.normpath(source)
-    basename_split = os.path.splitext(os.path.basename(source))
-    filename = basename_split[0]
-    extension = basename_split[1]
+    filename, extension = os.path.splitext(os.path.basename(source))
     if extension == '.shp':
         return QgsVectorLayer(source, filename, "ogr")
     elif extension == '.csv':
         return QgsVectorLayer(f'file:///{source}?type=csv&detectTypes=yes&geomType=none', filename, "delimitedtext")
 
 def get_fields(layer :QgsVectorLayer) -> list[str]:
+    """Get the fields of a vector layer.
+
+    Args:
+        layer (QgsVectorLayer): The layer to get the fields for.
+
+    Returns:
+        list[str]: A list of field names in the layer.
+    """
     if layer is not None:
         return layer.fields().names()
     return []
 
 def get_vector_layers() -> list[QgsVectorLayer]:
+    """Get all vector layers in the project.
+
+    Get all vector layers in the project that have the provider "ogr".
+
+    Returns:
+        list[QgsVectorLayer]: A list of vector layers in the project.
+    """
     layers: dict[str, QgsVectorLayer] = QgsProject.instance().mapLayers()
     vector_layers = []
     for layer in layers.values():
@@ -34,6 +55,13 @@ def get_vector_layers() -> list[QgsVectorLayer]:
     return vector_layers
 
 def join_layers(data_layer: QgsVectorLayer, target_layer: QgsVectorLayer, target_field: str):
+    """Join two layer attribute tables together.
+
+    Args:
+        data_layer (QgsVectorLayer): The layer to join from.
+        target_layer (QgsVectorLayer): The layer to join to.
+        target_field (str): The field in the target layer that will be used for joining.
+    """
     join = QgsVectorLayerJoinInfo()
     join.setJoinLayer(data_layer)
     join.setJoinFieldName('field_1')
@@ -42,25 +70,53 @@ def join_layers(data_layer: QgsVectorLayer, target_layer: QgsVectorLayer, target
     target_layer.addJoin(join)
 
 def add_to_project(*layers: QgsVectorLayer) -> None:
+    """Add one or more layers to the current QGIS project.
+
+    Args:
+        *layers (QgsVectorLayer): The layers to add to the project.
+    """
     for layer in layers:
         if not layer.isValid():
             QgsMessageLog.logMessage('Could not load the layer.')
         else: 
             QgsProject.instance().addMapLayer(layer)
 
-def add_to_group(layer: QgsVectorLayer, group_name: str) -> None:
-    root = QgsProject.instance().layerTreeRoot()
-    group = root.findGroup(group_name)
-    if group is None:
-        group = root.addGroup(group_name)
-    group.addLayer(layer)
+# def add_to_group(layer: QgsVectorLayer, group_name: str) -> None:
+#     """Add a layer to a group in the project.
+
+#     Args:
+#         layer (QgsVectorLayer): The layer to add to the group.
+#         group_name (str): The name of the group to add the layer to.
+#     """
+#     root = QgsProject.instance().layerTreeRoot()
+#     group = root.findGroup(group_name)
+#     if group is None:
+#         group = root.addGroup(group_name)
+#     group.addLayer(layer)
 
 def apply_style_file(source: str, map: QgsVectorLayer, attribute: str):
+    """Apply a style from a style file to a vector layer.
+
+    Args:
+        source (str): The path to the style file.
+        map (QgsVectorLayer): The layer to apply the style to.
+        attribute (str): The name of the attribute that the style is for.
+    """
     map.loadNamedStyle(source)
     map.renderer().setClassAttribute(attribute)
     map.triggerRepaint()
 
 def apply_default_attr_style(map_layer: QgsVectorLayer, class_attribute: str) -> None:
+    """Apply a default attribute style to the given map layer.
+    
+    If the class_attribute is recognized, this function will apply a pre-defined 
+    attribute style to the layer. Otherwise, it will fall back to applying a 
+    default symbology.
+    
+    Args:
+        map_layer (QgsVectorLayer): The layer object that will receive the style.
+        class_attribute (str): A string representing the attribute on which the style is based.
+    """
     layer_styles_folder = os.path.join(os.path.dirname(__file__), 'res', 'layer_styles')
     attribute_style_relation = {
         'C. Dk./LTA Pct.':          'rainfall_anomaly_percent_polygon5',
@@ -80,6 +136,18 @@ def apply_default_attr_style(map_layer: QgsVectorLayer, class_attribute: str) ->
     apply_default_symbology(map_layer, class_attribute)
 
 def apply_default_symbology(map_layer: QgsVectorLayer, class_attribute: str, nclasses=10):
+    """Apply a default symbology to the given map layer.
+    
+    This function uses the QgsGraduatedSymbolRenderer class to generate a 
+    default symbology for the layer, using the specified class_attribute as the 
+    classification method. The resulting symbology will have a gradient color 
+    ramp with three stops: 0.25 (white), 0.50 (gray), and 0.75 (black).
+
+    Args:
+        map_layer (QgsVectorLayer): The layer object that will receive the symbology.
+        class_attribute (str): A string representing the attribute on which the symbology is based.
+        nclasses (int): An integer representing the number of classes for the symbology.
+    """
     color_ramp_properties = {
         'color1':'255,255,255,255', 
         'stops':'0.25;192,192,192,255:0.50;128,128,128,255:0.75;64,64,64,255',
@@ -94,5 +162,13 @@ def apply_default_symbology(map_layer: QgsVectorLayer, class_attribute: str, ncl
     map_layer.setRenderer(renderer)
 
 def rename_layer(layer: QgsVectorLayer, name='', prefix='', suffix=''):
+    """Rename a vector layer.
+
+    Args:
+        layer (QgsVectorLayer): The layer to rename.
+        name (str, optional): The new name for the layer. Defaults to ''.
+        prefix (str, optional): A prefix to add to the new name. Defaults to ''.
+        suffix (str, optional): A suffix to add to the new name. Defaults to ''.
+    """
     if name == '': name = layer.name()
     layer.setName(prefix + name + suffix)
