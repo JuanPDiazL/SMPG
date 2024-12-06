d45f2924f7bebcfdde358e62e1500437cd909ef0
JuanPDiazL <juan.lombana@correounivalle.edu.co>
2024-12-06 10:20:42 -0500

    cleanup and fixes(python): replace attributes by variables and other fixes

--- qsmpgCore/structures.py
+++ qsmpgCore/structures.py
@@ -1,4 +1,4 @@
-from numpy import ndarray
+from numpy import ndarray, NaN
 import numpy as np
 import pandas as pd
 from .utils import *
@@ -67,75 +67,83 @@ class Place:
 
     Attributes:
         id (str): Unique identifier of the place.
-        timeseries (Series): Time series data for the place.
         parent (Dataset): Parent dataset that contains this place.
-        current_season (ndarray): Current season's data.
-        forecast_value (float or None): Forecast value in the current season.
+        current_season (Series): Current season's data.
         seasons_df (DataFrame): DataFrame of past seasons' data.
     """
         
     def __init__(self, place_id: str, timeseries: pd.Series, parent: Dataset) -> None:
         self.id = place_id
-        self.timeseries = timeseries
-        self.parent = parent
-        self.indexes = parent.properties.year_ids
-        self.columns = parent.properties.sub_season_ids
-        self.current_season = timeseries[parent.climatology_end_index : ].to_numpy()
-
+        indexes = parent.properties.year_ids
+        columns = parent.properties.sub_season_ids
+        self.current_season = timeseries[parent.climatology_end_index : ]
+        self.current_season.index = columns[:len(self.current_season)]
         past_seasons = timeseries[parent.season_shift : parent.climatology_end_index]
-        timeseries_reshaped = past_seasons.values.reshape(len(self.indexes), len(self.columns))
-        self.seasons_df = pd.DataFrame(timeseries_reshaped, index=self.indexes, 
-                                  columns=self.columns)
-        self.forecast_value = None
+        timeseries_reshaped = past_seasons.values.reshape(len(indexes), len(columns))
+        self.seasons_df = pd.DataFrame(timeseries_reshaped, index=indexes, 
+                                  columns=columns)
+        forecast_value = NaN
         if parent.parameters.is_forecast: 
-            self.forecast_value = self.current_season[-1]
+            forecast_value = self.current_season[-1]
             self.current_season = self.current_season[:-1]
-        self.current_season_monitoring = self.current_season[parent.season_start_index:parent.current_season_trim_index]
+        current_season_monitoring = self.current_season[parent.season_start_index:parent.current_season_trim_index]
         
-        self.similar_seasons = get_similar_years(self.current_season, 
+        self.similar_seasons = get_similar_years(self.current_season.to_numpy(), 
                                             self.seasons_df, 
                                             parent.parameters.use_pearson)
         if isinstance(parent.properties.selected_years, str):
-            self.selected_years = self.similar_seasons[:int(parent.properties.selected_years)]
+            selected_years = self.similar_seasons[:int(parent.properties.selected_years)]
         if isinstance(parent.properties.selected_years, list):
-            self.selected_years = parent.properties.selected_years
+            selected_years = parent.properties.selected_years
         
         # build seasonsal rainfall dataframes
-        self.seasonal_climatology = self.seasons_df.loc[self.parent.properties.climatology_year_ids]
+        self.seasonal_climatology = self.seasons_df.loc[parent.properties.climatology_year_ids]
         self.seasonal_monitoring = self.seasons_df.iloc[:, parent.season_start_index:parent.season_end_index]
-        self.seasonal_mon_sel = self.seasonal_monitoring.loc[self.selected_years]
-        self.seasonal_mon_clim = self.seasonal_monitoring.loc[self.parent.properties.climatology_year_ids]
+        self.seasonal_mon_sel = self.seasonal_monitoring.loc[selected_years]
+        self.seasonal_mon_clim = self.seasonal_monitoring.loc[parent.properties.climatology_year_ids]
         
         # calculate derived dataframes
-        self.current_cumsum = np.cumsum(self.current_season)
-        self.current_cumsum_mon = np.cumsum(self.current_season_monitoring)
-        self.current_index = len(self.current_season_monitoring) - 1
+        self.current_cumsum = self.current_season.cumsum()
+        self.current_cumsum_mon = current_season_monitoring.cumsum()
+        self.current_index = len(current_season_monitoring) - 1
 
         self.seasonal_cumsum = self.seasonal_monitoring.cumsum(axis=1)
-        self.seasonal_current_totals = self.seasonal_cumsum.iloc[:, self.current_index].to_numpy()
-        self.seasonal_ensemble = get_ensemble(self.current_season_monitoring, 
+        self.seasonal_current_totals = self.seasonal_cumsum.iloc[:, self.current_index].rename(self.id)
+        self.seasonal_ensemble = get_ensemble(current_season_monitoring, 
                                               self.seasonal_monitoring)
         self.selected_seasons_cumsum = self.seasonal_mon_sel.cumsum(axis=1)
-        self.selected_seasons_ensemble = get_ensemble(self.current_season_monitoring, 
+        self.selected_seasons_ensemble = get_ensemble(current_season_monitoring, 
                                                       self.seasonal_mon_sel)
         
         self.clim_seasons_cumsum = self.seasonal_mon_clim.cumsum(axis=1)
-        self.clim_seasons_ensemble = get_ensemble(self.current_season_monitoring, 
+        self.clim_seasons_ensemble = get_ensemble(current_season_monitoring, 
                                                   self.seasonal_mon_clim)
         
         # calculate stats
         self.clim_seasons_totals = self.clim_seasons_cumsum.iloc[:, -1].to_numpy()
         self.clim_seasons_pctls = percentiles_to_values(self.clim_seasons_totals, [33, 67])
+        self.seasonal_pctls = percentiles_to_values(self.seasonal_current_totals.to_numpy(), 
+                                                             (3, 6, 11, 21, 33, 67))
+        place_lt_stats = {
+            'Climatology Average': self.seasonal_climatology.mean(),
+            'Current Season': self.current_season,
+            'Current Season Accumulation': self.current_cumsum_mon,
+        }
+        self.place_long_term_stats = pd.DataFrame([pd.Series(v, name=k) for k, v in place_lt_stats.items()])
         self.place_general_stats = pd.Series({
-            'Current Season Pctl.': percentiles_from_values(self.seasonal_current_totals, 
-                                                            [self.current_cumsum_mon[-1]])[0],
-            'Drought Severity Pctls.': percentiles_to_values(self.seasonal_current_totals, 
-                                                             (3, 6, 11, 21, 33, 67)).tolist(),
-            'Pctls.': self.clim_seasons_pctls.tolist(),
-            'Avg.': self.seasonal_climatology.mean().to_list(),
-            'Current Season': self.current_season.tolist(),
-            'Current Season Accumulation': self.current_cumsum_mon.tolist(),
-            'Forecast': self.forecast_value,
+            'Current Season Pctl.': percentiles_from_values(self.seasonal_current_totals.to_numpy(), 
+                [self.current_cumsum_mon[-1]])[0],
+            'Current Season Total': self.current_cumsum_mon[-1],
+            'Seasonal 3 Pctl.': self.seasonal_pctls[0],
+            'Seasonal 6 Pctl.': self.seasonal_pctls[1],
+            'Seasonal 11 Pctl.': self.seasonal_pctls[2],
+            'Seasonal 21 Pctl.': self.seasonal_pctls[3],
+            'Seasonal 33 Pctl.': self.seasonal_pctls[4],
+            'Seasonal 67 Pctl.': self.seasonal_pctls[5],
+            'Climatology 33 Pctl.': self.clim_seasons_pctls[0],
+            'Climatology 67 Pctl.': self.clim_seasons_pctls[1],
+            'Forecast': forecast_value,
+            'Current Season+Forecast': self.current_cumsum_mon[-1] + forecast_value,
         }
         , name=self.id)
         self.seasonal_general_stats, self.seasonal_long_term_stats = \
@@ -174,6 +182,8 @@ class Place:
         standard_dev = seasonal_cumsum.std()
         seasonal_long_term_stats = pd.DataFrame({
             'LTA': seasonal_lta,
+            'LTA+20%': seasonal_lta*1.2,
+            'LTA-20%': seasonal_lta*.8,
             'Median': seasonal_ltm,
             'Ensemble Med.': ensemble_median,
             'E. LTA': ensemble_lta,
@@ -181,6 +191,10 @@ class Place:
         })
         seasonal_general_stats = pd.Series({
             'LTA': seasonal_lta[-1],
+            'LTA+St. Dev.': seasonal_lta[-1]+standard_dev[-1],
+            'LTA-St. Dev.': seasonal_lta[-1]-standard_dev[-1],
+            'E. LTA+St. Dev.': ensemble_lta[-1]+standard_dev[-1],
+            'E. LTA-St. Dev.': ensemble_lta[-1]-standard_dev[-1],
             'Median': seasonal_ltm[-1],
             'Ensemble Med.': ensemble_median[-1],
             'LTA up to Current Season': lta_upto_current_season,
@@ -188,8 +202,8 @@ class Place:
             'Ensemble Med./LTA': ensemble_median[-1]/seasonal_lta[-1],
             'Ensemble Med. Pctl.': percentiles_from_values(seasonal_totals, [ensemble_median[-1]])[0],
             'St. Dev.': standard_dev[-1],
-            '33 E. Pctl.': ensemble_pctls[0],
-            '67 E. Pctl.': ensemble_pctls[1],
+            'Ensemble 33 Pctl.': ensemble_pctls[0],
+            'Ensemble 67 Pctl.': ensemble_pctls[1],
             'E. Prob. Below Normal': ensemble_pctl_probabilities[0],
             'E. Prob. Between Normal': ensemble_pctl_probabilities[1],
             'E. Prob. Above Normal': ensemble_pctl_probabilities[2],
