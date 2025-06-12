60ea872b139f1e056e33d4202a7746074bd16765
JuanPDiazL <juan.lombana@correounivalle.edu.co>
2025-06-12 13:29:04 -0500

    added warnings for invalid data in input csv

--- smpgCore/parsers/CSVParser.py
+++ smpgCore/parsers/CSVParser.py
@@ -1,4 +1,6 @@
 import pandas as pd
+import numpy as np
+
 
 def parse_csv(filename:str):
     """Parse a CSV file into a pandas DataFrame and metadata.
@@ -12,6 +14,13 @@ def parse_csv(filename:str):
         has_duplicates (bool): Whether the resulting DataFrame contains duplicate rows.
     """
     df = pd.read_csv(filename, header=0, index_col=0)
+    
+    # detect invalid cells
+    if df.applymap(lambda x: not np.isreal(x)).any(axis=None):
+        raise ValueError('The CSV file contains invalid data.')
+    if df.applymap(np.isnan).any(axis=None):
+        raise ValueError('The CSV file has missing data.')
+
     has_duplicates = not df.index.is_unique
     df = df.groupby(level=0).first() # Remove duplicate rows
     timestamps = df.columns.to_list()
