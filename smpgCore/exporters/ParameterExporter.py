992e33537e70a2b48902ddb62ec55b801752d362
JuanPDiazL <juan.lombana@correounivalle.edu.co>
2026-03-11 09:21:34 -0500

    better parameters file readability

--- smpgCore/exporters/ParameterExporter.py
+++ smpgCore/exporters/ParameterExporter.py
@@ -15,7 +15,7 @@ def export_parameters(destination_path, dataset: Dataset, subFolderName='.'):
     """
     parameters_subfolder_path = os.path.join(destination_path, subFolderName)
     os.makedirs(parameters_subfolder_path, exist_ok=True)
-    json_data = json.dumps(dataset.parameters.__dict__)
+    json_data = json.dumps(dataset.parameters.__dict__, indent=4)
     os.makedirs(destination_path, exist_ok=True)
     with open(f'{parameters_subfolder_path}/Parameters.json', 'w') as js_data_wrapper:
         js_data_wrapper.write(json_data)
